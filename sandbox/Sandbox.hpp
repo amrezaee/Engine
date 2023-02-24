be00d546d73d8c98afc911e307e7d1eaf932110e
amrezaee <amrezaee2@gmail.com>
2023-02-24 17:31:36 +0330

    update sandbox

--- sandbox/Sandbox.hpp
+++ sandbox/Sandbox.hpp
@@ -14,34 +14,41 @@ public:
 
 		mCamera.SetProjection(0.0f, float(GetWindow().GetWidth()),
 		                      float(GetWindow().GetHeight()), 0.0f);
-		wp.x = GetWindow().GetWidth() * -0.5f + 10.0f;
-		wp.y = GetWindow().GetHeight() * -0.5f + 10.0f;
+
+		// GetWindow().SetWindowMode(WindowMode::BorderlessFS);
 	}
 
-	void Update(double dt) override {}
-
-	void Draw(double dt) override
+	void Update(double dt) override
 	{
+		wp.x = GetWindow().GetWidth() * -0.5f + 10.0f;
+		wp.y = GetWindow().GetHeight() * -0.5f + 10.0f;
+
 		GetRenderDevice().Clear();
 		GetRenderer().DrawBegin(mCamera);
 
-		for(U32 i = 0; i < 90; ++i)
+		for(U32 i = 0; i < 1000; ++i)
 			for(U32 j = 0; j < 100; ++j)
 				GetRenderer().DrawCircle({wp.x + (i * 20.0f), wp.y + (j * 20.0f)},
 				                         20.0f, Color::BURLYWOOD, 1.0f, 0.095f);
+
 		GetRenderer().DrawQuad({0.0f, 0.0f}, mSprite->GetResolution() * 10.0f,
 		                       mSprite);
 
 		GetRenderer().DrawEnd();
 
 		time += dt;
-		if(time > 0.8f)
+		if(time > 1.0f)
 		{
 			INFO("{},  {},  {:.4f},  {:.1f}",
 			     GetRenderer().GetFrameStats().DrawCalls,
 			     GetRenderer().GetFrameStats().QuadCount, dt, 1.0f / dt);
 			time = 0.0f;
 		}
+	}
+
+	void OnResize(Vec2ui resolution) override
+	{
+		mCamera.SetProjection(0.0f, resolution.x, resolution.y, 0.0f);
 	}
 
 private:
