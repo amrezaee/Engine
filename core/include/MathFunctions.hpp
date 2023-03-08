97195ef918f7bb630296968e9c7d59561a961934
amrezaee <amrezaee2@gmail.com>
2023-03-08 20:54:16 +0330

    remove Clamp, Max, Min and change constants types

--- core/include/MathFunctions.hpp
+++ core/include/MathFunctions.hpp
@@ -2,38 +2,20 @@
 
 #include <Common.hpp>
 
-constexpr float CPI      = 3.14159f;
-constexpr float CPI_180  = CPI / 180.0f;
-constexpr float C180_PI  = 180.0f / CPI;
-constexpr float CPI_SQRD = CPI * CPI;
-constexpr float CPI_HALF = CPI * 0.5f;
+constexpr real CPI      = 3.1415926535;
+constexpr real CPI_180  = CPI / 180.0;
+constexpr real C180_PI  = 180.0 / CPI;
+constexpr real CPI_SQRD = CPI * CPI;
+constexpr real CPI_HALF = CPI * 0.5;
 
-constexpr float Deg2Rad(const float deg)
+constexpr real Deg2Rad(real deg)
 {
 	return deg * CPI_180;
 }
 
-constexpr float Rad2Deg(const float rad)
+constexpr real Rad2Deg(real rad)
 {
 	return rad * C180_PI;
-}
-
-template<typename T>
-constexpr const T& Clamp(const T& v, const T& l, const T& h)
-{
-	return (v < l) ? l : ((h < v) ? h : v);
-}
-
-template<typename T>
-constexpr const T& Max(const T& a, const T& b)
-{
-	return (a < b) ? b : a;
-}
-
-template<typename T>
-constexpr const T& Min(const T& a, const T& b)
-{
-	return (a < b) ? a : b;
 }
 
 // clang-format off
