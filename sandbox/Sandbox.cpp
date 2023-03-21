f34d189495273f2f4d0fca85ecca7fbf545cb615
amrezaee <amrezaee2@gmail.com>
2023-03-21 22:03:48 +0330

    update sandbox

--- sandbox/Sandbox.cpp
+++ sandbox/Sandbox.cpp
@@ -4,3 +4,80 @@ UniquePtr<Application> CreateApp()
 {
 	return MakeUnique<Sandbox>();
 }
+
+Sandbox::Sandbox(): Application("Sandbox", ENGINE_ROOT_PATH)
+{
+}
+
+void Sandbox::Initialize()
+{
+	mRenderDevice->SetClearColor(Color::BLACK);
+	TexturePtr sprite = Texture::Create("assets/image.png");
+
+	mSceneManager.Add(MakeUnique<Scene>("Main"));
+	mSceneManager.Switch("Main");
+	Scene* scene = mSceneManager.GetCurrent();
+
+	{
+		Entity player = scene->CreateEntity("player");
+		player.AddComponent<SpriteRendererComponent>(sprite);
+		auto& t = player.GetComponent<TransformComponent>();
+		t.Scale = 8.0f;
+	}
+	// Vec2 wp;
+	// wp.x = mWindow->GetWidth() * -0.5f + 10.0f;
+	// wp.y = mWindow->GetHeight() * -0.5f + 10.0f;
+
+	{
+		Entity circle = scene->CreateEntity("circle0");
+		circle.AddComponent<CircleRendererComponent>();
+		auto& t      = circle.GetComponent<TransformComponent>();
+		t.Position.x = 100;
+		auto& s      = circle.GetComponent<CircleRendererComponent>();
+		s.Smoothness = 0.1f;
+		s.Color      = Color::INDIGO;
+	}
+
+	{
+		Entity circle = scene->CreateEntity("circle1");
+		circle.AddComponent<CircleRendererComponent>();
+		auto& t      = circle.GetComponent<TransformComponent>();
+		t.Position.x = -100.0f;
+		auto& s      = circle.GetComponent<CircleRendererComponent>();
+		s.Smoothness = 0.1f;
+		s.Color      = Color::YELLOW_GREEN;
+	}
+
+	{
+		Entity circle = scene->CreateEntity("circle2");
+		circle.AddComponent<CircleRendererComponent>();
+		auto& t      = circle.GetComponent<TransformComponent>();
+		t.Position.y = 100.0f;
+		auto& s      = circle.GetComponent<CircleRendererComponent>();
+		s.Smoothness = 0.1f;
+		s.Color      = Color::CHOCOLATE;
+	}
+
+	{
+		Entity circle = scene->CreateEntity("circle3");
+		circle.AddComponent<CircleRendererComponent>();
+		auto& t      = circle.GetComponent<TransformComponent>();
+		t.Position.y = -100.0f;
+		auto& s      = circle.GetComponent<CircleRendererComponent>();
+		s.Smoothness = 0.1f;
+		s.Color      = Color::DEEP_PINK;
+	}
+}
+
+void Sandbox::Update(double dt)
+{
+	mRenderDevice->Clear();
+
+	time += dt;
+	if(time > 1.0f)
+	{
+		INFO("{},  {},  {:.4f},  {:.1f}", mRenderer->GetFrameStats().DrawCalls,
+		     mRenderer->GetFrameStats().QuadCount, dt, 1.0f / dt);
+		time = 0.0f;
+	}
+}
--- sandbox/Sandbox.hpp
+++ sandbox/Sandbox.hpp
@@ -5,48 +5,10 @@
 class Sandbox final: public Application
 {
 public:
-	Sandbox(): Application("Sandbox", ENGINE_ROOT_PATH) {}
-
-	void Initialize() override
-	{
-		mRenderDevice->SetClearColor(Color::BLACK);
-		TexturePtr sprite = Texture::Create("assets/image.png");
-
-		mSceneManager.Add(MakeUnique<Scene>("Main"));
-		mSceneManager.Switch("Main");
-		Scene* scene = mSceneManager.GetCurrent();
-		player       = scene->CreateEntity("player");
-		player.AddComponent<SpriteRendererComponent>(sprite);
-		auto& t = player.GetComponent<TransformComponent>();
-		t.Scale = 8.0f;
-	}
-
-	void Update(double dt) override
-	{
-		mRenderDevice->Clear();
-
-		//wp.x = mWindow->GetWidth() * -0.5f + 10.0f;
-		//wp.y = mWindow->GetHeight() * -0.5f + 10.0f;
-		
-		/*mRenderer->DrawBegin(mCamera);
-
-		for(uword i = 0; i < 100; ++i)
-			for(uword j = 0; j < 100; ++j)
-				mRenderer->DrawCircle({wp.x + (i * 20.0f), wp.y + (j * 20.0f)},
-				                      20.0f, Color::BURLYWOOD, 1.0f, 0.095f);
-		mRenderer->DrawEnd();*/
-
-		time += dt;
-		if(time > 1.0f)
-		{
-			INFO("{},  {},  {:.4f},  {:.1f}", mRenderer->GetFrameStats().DrawCalls,
-			     mRenderer->GetFrameStats().QuadCount, dt, 1.0f / dt);
-			time = 0.0f;
-		}
-	}
+	Sandbox();
+	void Initialize() override;
+	void Update(double dt) override;
 
 private:
-	Vec2       wp;
-	float      time {0.0f};
-	Entity     player;
+	float  time {0.0f};
 };
